7d254608477564b1a959716b806bd38132876670
jorel000 <joshua.jorel.lee@gmail.com>
2012-07-16 15:10:15 +0800

    Created

---
+++ Objects/Physician.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace introseHHC.Objects
+{
+    class Physician:Person
+    {
+        private string discipline;
+        private int id;
+    }
+}
